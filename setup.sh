bc39686e2c95552e3ed1c02a8b4370c6206b5343
pooas <cyroustrader@gmail.com>
2026-06-21 12:47:26 +0330

    Add offline clickhouse plugin and update docker-compose

--- setup.sh
+++ setup.sh
@@ -1,29 +1,38 @@
 #!/bin/bash
 
-# توقف خودکار اسکریپت در صورت بروز هرگونه ارور
+# توقف در صورت ارور
 set -e
 
-echo "➡️ Starting deployment process..."
+echo "➡️ Stopping and cleaning up previous containers..."
+# اگر پوشه از قبل وجود داشته باشد، کانتینرها و والیوم‌های آن را متوقف و پاک می‌کند
+if [ -d "HFTBinanceSSA" ]; then
+  cd HFTBinanceSSA
+  sudo docker compose down -v || true
+  cd ..
+  sudo rm -rf HFTBinanceSSA
+fi
 
-# ۱. کلون کردن ریپازیتوری با استفاده از توکن
 echo "➡️ Cloning the repository..."
-# در صورت وجود پوشه از قبل، آن را پاک می‌کند تا تداخل ایجاد نشود
-rm -rf HFTBinanceSSA
 git clone https://pooas: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@github.com/pooas/HFTBinanceSSA.git
 
-# ورود به پوشه پروژه
 cd HFTBinanceSSA
 
-# ۲. نصب پیش‌نیازها و کلیدهای داکر
-echo "➡️ Installing Docker prerequisites..."
+# حل مشکل خوانده نشدن پلاگین گرافانا
+if [ -d "grafana-plugins" ]; then
+    echo "➡️ Fixing permissions for Grafana offline plugins..."
+    # تغییر مالکیت پوشه به یوزر گرافانا (شناسه 472)
+    sudo chown -R 472:472 grafana-plugins
+    sudo chmod -R 775 grafana-plugins
+fi
+
+# نصب پیش‌نیازها و داکر (اگر نصب نباشند انجام می‌شود)
+echo "➡️ Checking and configuring Docker..."
 sudo apt update -y
 sudo apt install -y ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc || true
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-# ۳. اضافه کردن سورس داکر به اوبونتو
-echo "➡️ Adding Docker repository..."
 sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
 Types: deb
 URIs: https://download.docker.com/linux/ubuntu
@@ -33,20 +42,13 @@ Architectures: $(dpkg --print-architecture)
 Signed-By: /etc/apt/keyrings/docker.asc
 EOF
 
-# ۴. نصب پکیج‌های اصلی داکر
-echo "➡️ Installing Docker packages..."
 sudo apt update -y
 sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-# استارت کردن و فعال‌سازی داکر برای اجرای خودکار بعد از ریبوت سرور
-echo "➡️ Starting Docker service..."
 sudo systemctl enable docker
 sudo systemctl start docker
 
-# ۵. اجرای کانتینرها
+# استارت کردن پروژه
 echo "➡️ Running docker compose..."
 sudo docker compose up -d
 
-echo "✅ Deployment completed successfully! HFT system is running."
-
-docker logs -f hft_java_producer
+echo "✅ Deployment completed successfully! Grafana should now load the plugin."
