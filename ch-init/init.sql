93f9304fca219609c8d53053927bc0c4459f979a
pooas <cyroustrader@gmail.com>
2026-06-27 11:39:06 +0330

    feat: change ssa idea in java in order to make more smooth

--- ch-init/init.sql
+++ ch-init/init.sql
@@ -18,7 +18,9 @@ CREATE TABLE default.hft_market_data (
     hmm_regime Int8,
     hmm_prob_trend Float64,
     hmm_prob_crisis Float64,
-    
+    value2 Float64،
+    dom_cycle Float64،
+
     -- 🌟 ستون‌های خروجی قانون بیز و استراتژی مقاله
     momentum_signal Float64,      
     regime_weight Float64,        
@@ -30,3 +32,4 @@ CREATE TABLE default.hft_market_data (
 PARTITION BY toYYYYMMDD(timestamp)
 ORDER BY (timestamp, sequence)
 SETTINGS index_granularity = 8192;
+
